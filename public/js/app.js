57ce2cb9d021e9f9cd8e3b1fee597c4635b547a1
Rubina Pradhan <pradhanr03@gmail.com>
2015-08-08 00:40:25 -0400

    rp

--- public/js/app.js
+++ public/js/app.js
@@ -7,12 +7,14 @@ $(document).ready(function() {
  		 
 
         $(".feature").on("click", function(){  
+        	$("#contribute").empty();
             $("#globe").html('<object data="https://en.wikipedia.org/wiki/Portal:Featured_content"/>');
             
         });
 
 
-        $(".random").on("click", function(){  
+        $(".random").on("click", function(){ 
+        	$("#contribute").empty(); 
             $("#globe").html('<object data="http://en.wikipedia.org/wiki/Special:Random"/>');
             
         });
